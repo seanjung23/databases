b2169fe61553aa1d6b33f62ab62e35aaa3574d2d
Sean Jung <sean.jung23@gmail.com>
2023-04-20 15:15:23 -0400

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,42 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id int AUTO_INCREMENT,
+  roomname text,
+  PRIMARY KEY (id)
 );
 
+CREATE TABLE users (
+  id int AUTO_INCREMENT,
+  username text,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id int AUTO_INCREMENT,
+  texts text,
+  room_id int,
+  username_id int,
+  PRIMARY KEY (id),
+  -- add a room column here
+  FOREIGN KEY (room_id) REFERENCES rooms(id),
+  FOREIGN KEY (username_id) REFERENCES users(id)
+);
+
+
+
+-- insert integero chat (room_name) values``
+--   ('Main')
+--   ('Secondary');
+
+-- insert integero users ()
+
+-- insert integero chat (message, room, username) values
+--   ()
+
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
