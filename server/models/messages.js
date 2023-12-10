cd7fd2ea4adbae4fb9a3aeb04d27f2d429dd1a69
Sean <sean.jung23@gmail.com>
2023-12-10 16:17:21 -0500

    message/user models complete

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,16 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
-  create: function () {} // a function which can be used to insert a message into the database
+  getAll: function (callback) {
+    var queryStr = 'select messages.id, messages.text, messages.roomname users.username from messages left outer join users on (messages.userid = users.id order by messages.id desc';
+    db.query(queryStr, function (err, results) => {
+      callback(results);
+    });
+  }, // a function which produces all the messages
+  create: function (params, callback) {
+    var queryStr = 'insert into messages(text, userid, roomname) values (?, (select id from users where username = ? limit 1), ?)';
+    db.query(queryStr, params function (err, results) => {
+      callback(results);
+    });
+  } // a function which can be used to insert a message into the database
 };
--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,16 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {},
-  create: function () {}
+  getAll: function (callback) {
+    var queryStr = '';
+    db.query(queryStr, function (err, results) => {
+      callback(results);
+    });
+  },
+  create: function (params, callback) {
+    var queryStr = 'insert into users(username) values(?)';
+    db.query(queryStr, params function (err, results) => {
+      callback(results);
+    });
+  }
 };
