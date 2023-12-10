19c4b709ff0afcc73d5230cdb229e1907d843ea2
Sean <sean.jung23@gmail.com>
2023-12-10 16:17:01 -0500

    schema complete

--- server/schema.sql
+++ server/schema.sql
@@ -2,42 +2,31 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  text varchar(200) NOT NULL,
+  roomname varchar(20),
+  PRIMARY KEY (ID),
+  -- add a room column here
+  FOREIGN KEY (room_id) REFERENCES rooms(id),
+  FOREIGN KEY (username_id) REFERENCES users(id)
+);
+
+/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(40) NOT NULL,
+  PRIMARY KEY (ID)
+);
+
 CREATE TABLE rooms (
   id int AUTO_INCREMENT,
   roomname text,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE users (
-  id int AUTO_INCREMENT,
-  username text,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
-  id int AUTO_INCREMENT,
-  texts text,
-  room_id int,
-  username_id int,
-  PRIMARY KEY (id),
-  -- add a room column here
-  FOREIGN KEY (room_id) REFERENCES rooms(id),
-  FOREIGN KEY (username_id) REFERENCES users(id)
-);
-
-
-
--- insert integero chat (room_name) values``
---   ('Main')
---   ('Secondary');
-
--- insert integero users ()
-
--- insert integero chat (message, room, username) values
---   ()
-
-/* Create other tables and define schemas for them here! */
 
 
 /*  Execute this file from the command line by typing:
